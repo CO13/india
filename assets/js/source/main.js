04b0d5c95558c8980c5706964574756ca0c91dae
ok13 <cebanoctavian13@gmail.com>
2017-07-25 10:15:16 +0300

    modif detail v4

--- assets/js/source/main.js
+++ assets/js/source/main.js
@@ -1,6 +1,42 @@
 
 jQuery(document).ready( function($) {
 
+    //Add rating
+    (function() {
+
+        jQuery(document).on('click', '.add-review .review-star i', function() {
+            var selected = jQuery(this).index();
+            var star = jQuery(this).closest('.review-star').find('i');
+
+            jQuery(this).closest('.review-star').find('input[name="review-rating"]').val(selected);
+
+            star.removeClass('full');
+            jQuery.each(star, function(i) {
+                if((i + 1) <= selected) {
+                    jQuery(this).addClass('full');
+                }
+            });
+        });
+
+        jQuery(document).on({
+        		mouseenter: function(){
+                var star = jQuery(this).closest('.add-review .review-star').find('i');
+                var selected = jQuery(this).index();
+
+                jQuery.each(star, function(i) {
+                    if((i + 1) <= selected) {
+                        jQuery(this).addClass('hover');
+                    }
+                });
+
+        		},
+        		mouseleave: function(){
+                jQuery('.add-review  .review-star i').removeClass('hover');
+        		}
+        	}, '.add-review .review-star i');
+
+    }());
+
     // Initialize home products slider
     $('.slider-product').not('.slick-initialized').slick({
         slidesToShow: 5,
